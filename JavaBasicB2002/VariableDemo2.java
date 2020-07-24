7e6d5d8ad749c433fd87aacc4a827b8ee012c1e1
ren6728 <hreng6728@gmail.com>
2020-07-23 21:55:02 -0400

    added 2nd class

---
+++ JavaBasicB2002/VariableDemo2.java
@@ -0,0 +1,28 @@
+//VariableDemo2.java
+public class VariableDemo2{
+    public static void main(String[] args){
+        System.out.println("I've got nice dinner tonight!");
+        int myNum=5;
+        float myFloatNum=5.9912345f;
+        System.out.print(myNum + myFloatNum);
+
+        char MyLetter ='D', a=65,b=66,c=67;
+        System.out.println(MyLetter + a + b + c);
+
+        boolean myBool= true;
+        String myText="Hello!";
+        byte myNum2=100;
+        System.out.println(myNum2 + myText + myBool);
+
+        short myNum3 =5000;
+        System.out.println(myNum3);
+
+        long myNum4=1500000000L;
+        System.out.println(myNum4);
+        
+        double myNum5=19.987654321012345d;
+        System.out.println(myNum5);
+
+        
+    }
+}
