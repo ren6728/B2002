406010d030f2dbb237e04b5fec2c7161c6092f8a
ren6728 <hreng6728@gmail.com>
2020-07-26 17:11:28 -0400

    added C20 catch

---
+++ JavaBasicB2002/VariableDemo20.java
@@ -0,0 +1,16 @@
+public class VariableDemo20{
+    public static void main(String[] args){
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);     
+System.out.println(isFishTasty);
+  byte myNum = 100;
+  System.out.println(myNum);
+  try {
+  int[] myNumbers = {1, 2, 3};
+  System.out.println(myNumbers[10]);
+} catch (Exception e) {
+  System.out.println("Something went wrong.");
+}
+    }
+    }
