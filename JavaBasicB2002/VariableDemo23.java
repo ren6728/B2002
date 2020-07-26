335b253cac458397efe2a766331ff3ada4482fc7
ren6728 <hreng6728@gmail.com>
2020-07-26 17:29:12 -0400

    added C23 do, else

---
+++ JavaBasicB2002/VariableDemo23.java
@@ -0,0 +1,21 @@
+public class VariableDemo23{
+    public static void main(String[] args){
+    double myNum = 19.99d;
+System.out.println(myNum);
+    int i = 0;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 5);
+   int time = 22;
+   System.out.println("-----------------------------");
+if (time < 10) {
+  System.out.println("Good morning.");
+} else if (time < 20) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+} 
+    }
+}
