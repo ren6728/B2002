91586bd1760d7a041294aabd9fb1be8b16ddcb1c
ren6728 <hreng6728@gmail.com>
2020-07-26 17:03:00 -0400

    added C19 with object

---
+++ JavaBasicB2002/VariableDemo19.java
@@ -0,0 +1,15 @@
+public class VariableDemo19{
+    public static void main(String[] args){
+    VariableDemo18 obj = new VariableDemo18();
+    System.out.println("Name: " + obj.fname);
+    System.out.println("Age: " + obj.age);
+    System.out.println("Grade: " + obj.grade);
+    System.out.println("myNum: " + obj.myNum);
+    System.out.println("myDouble: " + obj.myDouble);
+    System.out.println("myFloatNum: " + obj.myFloatNum);
+    System.out.println("Graduation Year: " + obj.graduationYear);
+    System.out.println("--------------------"); 
+    System.out.println("----------****----------");
+    obj.study(); 
+    }
+}
