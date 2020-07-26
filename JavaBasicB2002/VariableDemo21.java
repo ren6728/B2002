c9e83fde59af093dacc2f6867193d66e239bb65e
ren6728 <hreng6728@gmail.com>
2020-07-26 17:19:41 -0400

    added C21 char,break,continue

---
+++ JavaBasicB2002/VariableDemo21.java
@@ -0,0 +1,21 @@
+public class VariableDemo21{
+    public static void main(String[] args){
+   System.out.println("Hello World");
+   char myGrade = 'B';
+   System.out.println(myGrade);
+   int i = 0;
+   while (i < 15) {
+   System.out.println(i);
+   i++;
+   if (i == 3) {
+   break;
+  }
+}
+  /* for (int ii = 0; i < 10; ii++) {
+   if (ii == 4) {
+    continue;
+  }
+    System.out.println(ii);
+} */
+    }
+}
