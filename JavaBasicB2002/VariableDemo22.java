e64467ed902535895c5fcf1de8491a4cf2a42df8
ren6728 <hreng6728@gmail.com>
2020-07-26 17:24:04 -0400

    added C22 case,break,default

---
+++ JavaBasicB2002/VariableDemo22.java
@@ -0,0 +1,15 @@
+public class VariableDemo22{
+    public static void main(String[] args){
+int day = 1;
+switch (day) {
+  case 6:
+    System.out.println("Today is Saturday");
+    break;
+  case 7:
+    System.out.println("Today is Sunday");
+    break;
+  default:
+    System.out.println("Looking forward to the Weekend");
+}
+    }
+}
