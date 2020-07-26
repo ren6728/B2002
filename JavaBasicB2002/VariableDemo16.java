9f5d154572157a9ef51fdd889ed41f6611da13cc
ren6728 <hreng6728@gmail.com>
2020-07-26 16:09:53 -0400

    added class16 with Math.max,String,char

---
+++ JavaBasicB2002/VariableDemo16.java
@@ -0,0 +1,25 @@
+public class VariableDemo16{
+public static void main(String[] args){  
+   Math.max(5, 15);
+   Math.min(5, 18);
+   Math.sqrt(36);
+   Math.abs(-4.7);
+   Math.random();
+   System.out.println(Math.max(5, 15));
+   System.out.println(Math.min(5, 18));
+   System.out.println(Math.sqrt(36));
+   System.out.println(Math.abs(-4.7));
+   System.out.println(Math.random());
+   System.out.println("**********break1************");
+
+   int randomNum = (int)(Math.random() * 101);
+   System.out.println(randomNum);
+   String nameOfLatePerson = "Kiran";
+   char kiranGrade ='F';
+   System.out.println(nameOfLatePerson + "is : " + kiranGrade);
+   System.out.println("**********break2************");
+
+
+}
+
+}
