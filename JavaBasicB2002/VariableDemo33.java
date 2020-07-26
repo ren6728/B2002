e236927e4ad138d036447934e5d05934b301fe5c
ren6728 <hreng6728@gmail.com>
2020-07-26 19:41:21 -0400

    added C33 NEW,C34 Package not executed yet

---
+++ JavaBasicB2002/VariableDemo33.java
@@ -0,0 +1,14 @@
+public class VariableDemo33{
+     int x = 5;
+    String name= "ren";
+    short grade= 'A';
+    float a=0.55f;
+public static void main(String[] args) {
+    VariableDemo33 myObj = new VariableDemo33();
+    System.out.println(myObj.x);
+    System.out.println(myObj.name);
+    System.out.println("**************");
+    System.out.println(myObj.grade);
+    System.out.println(myObj.a);
+}
+}
---
+++ JavaBasicB2002/VariableDemo34.java
@@ -0,0 +1,16 @@
+//package mypack;
+public class VariableDemo34{
+    int x = 5;
+    String name= "ren";
+    short grade= 'A';
+    float a=0.55f;
+public static void main(String[] args) {
+    System.out.println("This is my package!"); 
+    VariableDemo34 myObj = new VariableDemo34();
+    System.out.println(myObj.x);
+    System.out.println(myObj.name);
+    System.out.println("**************");
+    System.out.println(myObj.grade);
+    System.out.println(myObj.a);
+}
+ }
