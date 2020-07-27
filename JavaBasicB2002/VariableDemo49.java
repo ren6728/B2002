c1ef25646f250983b80cffb63284df3e417162d5
ren6728 <hreng6728@gmail.com>
2020-07-26 22:01:04 -0400

    added C49 LinkedList

---
+++ JavaBasicB2002/VariableDemo49.java
@@ -0,0 +1,17 @@
+// Import the LinkedList class
+import java.util.LinkedList;
+
+public class VariableDemo49 {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    cars.add("Audi");
+    cars.add("Toyota");
+    System.out.println(cars);
+    //System.out.println(cars.length);
+  }
+}
+
