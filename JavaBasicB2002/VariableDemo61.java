4bb77570c136b6820f9e3c14d590864861014394
ren6728 <hreng6728@gmail.com>
2020-07-27 00:04:03 -0400

    added C60 Widening casting,C61 narrowing casting

---
+++ JavaBasicB2002/VariableDemo61.java
@@ -0,0 +1,9 @@
+public class VariableDemo61{
+  public static void main(String[] args) {
+    double myDouble = 9.78;
+    int myInt = (int) myDouble; // Explicit casting: double to int
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
