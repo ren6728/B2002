17f1fe2474d9eaca87af11f7004834576ade4018
ren6728 <hreng6728@gmail.com>
2020-07-26 21:20:38 -0400

    added C42 throw

---
+++ JavaBasicB2002/VariableDemo42.java
@@ -0,0 +1,16 @@
+public class VariableDemo42 {
+  static void checkAge(int age) {
+    if (age < 18) {
+      throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+    }else if (age>16){
+      throw new ArithmeticException("Access granted one time! ");
+    }
+    else {
+      System.out.println("Access granted - You are old enough!");
+    }
+  }
+
+  public static void main(String[] args) {
+    checkAge(15); // Set age to 15 (which is below 18...)
+  }
+}
