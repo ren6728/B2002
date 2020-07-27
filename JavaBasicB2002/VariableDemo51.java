0ff7bb44ace87e019f4275e3e8d08bdd094d2fd2
ren6728 <hreng6728@gmail.com>
2020-07-26 22:19:32 -0400

    added C51 object of class

---
+++ JavaBasicB2002/VariableDemo51.java
@@ -0,0 +1,16 @@
+public class VariableDemo51 {
+  int x = 5;
+  double y =1.9903245678;
+  short z=5000;
+  byte a= 15;
+  float b= 0.12345f;
+
+  public static void main(String[] args) {
+    VariableDemo51 myObj = new VariableDemo51();
+    System.out.println(myObj.x);
+    System.out.println(myObj.y);
+    System.out.println(myObj.z);
+    System.out.println(myObj.a);
+    System.out.println(myObj.b);
+  }
+}
