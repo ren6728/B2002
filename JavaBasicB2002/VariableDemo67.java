2a5542351f14224cbc8c0c7997540ce1e4afb768
ren6728 <hreng6728@gmail.com>
2020-07-27 00:35:42 -0400

    added  C67 multidisplay

---
+++ JavaBasicB2002/VariableDemo67.java
@@ -0,0 +1,15 @@
+public class VariableDemo67 {
+  public static void main(String[] args) {
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);
+    String name = "John";
+    System.out.println("Hello " + name);
+    int myNum = 15;
+    myNum = 20;  // myNum is now 20
+    System.out.println(myNum);
+  }
+}
