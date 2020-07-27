a825ef3443907f057a7906f5f0514916d86f5039
ren6728 <hreng6728@gmail.com>
2020-07-27 00:30:39 -0400

    added C66 comments

---
+++ JavaBasicB2002/VariableDemo66.java
@@ -0,0 +1,13 @@
+public class VariableDemo66 {
+  public static void main(String[] args) {
+    // This is a comment
+    System.out.println("-------------");
+    System.out.println("Hello World");
+     System.out.println("***************");
+    System.out.println("Hello friend"); // This is a comment
+  /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+    System.out.println("*******end********");
+    System.out.println("Helloooo");
+  }
+}
