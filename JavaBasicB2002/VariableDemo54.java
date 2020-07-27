b027a2ff2d8adf7785493ad1a1fe4347a4c6bc9c
ren6728 <hreng6728@gmail.com>
2020-07-26 22:32:33 -0400

    added C53 non/static, C54 access method with object

---
+++ JavaBasicB2002/VariableDemo54.java
@@ -0,0 +1,23 @@
+// Create a class
+public class VariableDemo54 {
+ 
+  // Create a fullThrottle() method
+  public void fullThrottle() {
+    System.out.println("The car is going as fast as it can!");
+  }
+
+  // Create a speed() method and add a parameter
+  public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
+
+  // Inside main, call the methods on the  object
+  public static void main(String[] args) {
+    VariableDemo54 myCar = new VariableDemo54();     // Create a myCar object
+    myCar.fullThrottle();      // Call the fullThrottle() method
+    myCar.speed(200);          // Call the speed() method
+  }
+}
+
+// The car is going as fast as it can!
+// Max speed is: 200
